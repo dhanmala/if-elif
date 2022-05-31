59d1c4edc7a61ec2a7e7b36043daf62f073efa80
dhanmala <106421253+dhanmala@users.noreply.github.com>
2022-05-31 09:20:46 +0530

    Add files via upload

---
+++ age mf.py
@@ -0,0 +1,10 @@
+m=int(input("enter the age"))
+f=int(input("enter the age"))
+if m>=18 and f<=30:
+    wages=700*750 
+    print("wages of male and female is appropriate")
+elif m>=30 and f<=40:
+    wages=700*750
+    print("wages of female and male is high ")
+else:
+    print("none")        
